84703a9e662dd0c7a81d09eaea6db5b5e09d2782
GanaviSwamy <sonups2005@gmail.com>
2026-02-27 16:30:53 +0530

    Add files via upload

---
+++ Module 1/gradecalculator.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter marks (0-100): ");
+        int marks = sc.nextInt();
+
+        int gradeCategory = marks / 10;   // To group marks (90-100, 80-89, etc.)
+
+        switch (gradeCategory) {
+            case 10:
+            case 9:
+                System.out.println("Grade: A");
+                break;
+
+            case 8:
+                System.out.println("Grade: B");
+                break;
+
+            case 7:
+                System.out.println("Grade: C");
+                break;
+
+            case 6:
+                System.out.println("Grade: D");
+                break;
+
+            default:
+                if (marks >= 0 && marks < 60)
+                    System.out.println("Grade: F");
+                else
+                    System.out.println("Invalid marks entered.");
+        }
+
+        sc.close();
+    }
+}
---
+++ Module 1/sumofevennumbers.java
@@ -0,0 +1,15 @@
+public class SumOfEvenNumbers {
+    public static void main(String[] args) {
+        int i = 1;
+        int sum = 0;
+
+        while (i <= 100) {
+            if (i % 2 == 0) {   // Check if number is even
+                sum += i;
+            }
+            i++;
+        }
+
+        System.out.println("Sum of even numbers between 1 and 100 is: " + sum);
+    }
+}
